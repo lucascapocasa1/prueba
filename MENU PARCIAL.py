e17cebbbcdd3a5adcb33b2e2b844cdec0a09a569
lucascapocasa1 <86982967+lucascapocasa1@users.noreply.github.com>
2021-07-05 17:06:50 -0300

    Add files via upload

---
+++ MENU PARCIAL.py
@@ -0,0 +1,48 @@
+# MENU ##
+
+opcion = input("Pulse 1 para elegir duracion, 2 para configurar probabilidades, 3 para iniciar partida, 4 para ver el ranking, o 5 para salir.")
+
+flag_salir = False
+
+while not flag_salir:
+    #while opcion != 5:
+
+    if opcion == "1":
+
+        pass
+
+        opcion = input("Pulse 1 para elegir duracion, 2 para configurar probabilidades, 3 para iniciar partida, 4 para ver el ranking, o 5 para salir.")
+
+
+    elif opcion == "2":
+
+        pass
+
+        opcion = input("Pulse 1 para elegir duracion, 2 para configurar probabilidades, 3 para iniciar partida, 4 para ver el ranking, o 5 para salir.")
+
+    elif opcion == "3":
+
+        pass
+           
+        opcion = input("Pulse 1 para elegir duracion, 2 para configurar probabilidades, 3 para iniciar partida, 4 para ver el ranking, o 5 para salir.")
+
+    elif opcion == "4":
+
+        pass
+
+        opcion = input("Pulse 1 para elegir duracion, 2 para configurar probabilidades, 3 para iniciar partida, 4 para ver el ranking, o 5 para salir.")
+
+    else opcion == "5":
+
+        flag_salir = True 
+
+
+
+
+
+
+
+# VALIDACION ##
+
+
+
